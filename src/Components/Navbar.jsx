bcb7cde74141c3ca903f4e65ec54ef0766254f8c
preshpi <egwuenuprecious7@gamil.com>
2023-01-07 21:27:17 +0100

    fix: Navbar links and styles

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { FiSearch, FiShoppingCart, FiBell } from "react-icons/fi";
 const Navbar = () => {
@@ -7,41 +7,42 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
   const handleClose = () => setNav(!nav);
 
-  return (
-    <div className="w-[100%] h-[80px] z-10">
-      <div className="md:px-8 flex place-items-center justify-between items-center mt-[20px] px-4">
 
+  return (
+    <div className="w-[100%] h-[80px] z-10 lg:mt-[52px]">
+      <div className="md:px-8 flex place-items-center justify-between items-center mt-[20px]">
         {/* Menubar*/}
         <div className="md:hidden" onClick={handleClick}>
-            {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         </div>
 
-          {/* Logo */}
+        {/* Logo */}
         <div className="flex items-center ">
-          <div className="font-bold">
-            <h1 className="lg:text-3xl md:text-2xl mr-4 text-[24px] font-[sentient]">
+          <div className="font-bold flex justify-center items-center">
+            <h1 className="lg:text-3xl md:text-2xl text-[24px] font-[sentient] text-center ">
               ARSTY.
             </h1>
-          </div> </div>
+          </div>{" "}
+        </div>
 
-          {/* Nav links */}
-        <ul className="hidden md:flex lg:gap-[47px] md:gap-[20px]">
-          <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px]  text-[#292929] w-[65px] h-[32px]">
-            <Link to="/">Home</Link>
+        {/* Nav Navlinks */}
+        <ul className="hidden md:flex lg:gap-[47px] md:gap-[10px]">
+          <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[16px]  text-[#292929">
+            <NavLink to="/">Home</NavLink>
           </li>
-          <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px] text-[#292929] w-[129px] h-[32px]">
-            <Link to="/marketPlace">MarketPlace</Link>
+          <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[16px] text-[#292929]">
+            <NavLink to="/marketPlace">MarketPlace</NavLink>
           </li>
-          <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px] text-[#292929] w-[91px] h-[32px]">
-            <Link to="/auction">Auctions</Link>
+          <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[16px] text-[#292929]">
+            <NavLink to="/auction">Auctions</NavLink>
           </li>
-          <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px] text-[#292929] w-[53px] h-[32px]">
-            <Link to="/drop">Drop</Link>
+          <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[16px] text-[#292929]">
+            <NavLink to="/drop">Drop</NavLink>
           </li>
         </ul>
 
-          {/* Icons */}
-        <ul className="flex lg:gap-[31px] md:gap-[20px] gap-[17px] px-4">
+        {/* Icons */}
+        <ul className="flex lg:gap-[31px] md:gap-[10px] gap-[12px]">
           <li>
             <FiSearch className="cursor-pointer lg:w-[28px] lg:h-[28px] w-[16px]" />
           </li>
@@ -49,13 +50,12 @@ const Navbar = () => {
             <FiShoppingCart className="cursor-pointer lg:w-[28px] lg:h-[28px] w-[17px]" />
           </li>
           <li className="hidden md:flex">
-            <Link to="/drop">
+            <NavLink to="/drop">
               <FiBell className="cursor-pointer lg:w-[28px]  lg:h-[28px] w-[20px]" />
-            </Link>
+            </NavLink>
           </li>
         </ul>
       </div>
-      
 
       {/* Mobile nav */}
       <ul
@@ -66,29 +66,37 @@ const Navbar = () => {
         {/* Menubar*/}
         <div className="flex justify-between mt-[20px]">
           <div className="flex items-center ">
-          <div className="lg:pl-[120px] font-bold">
-            <h1 className="lg:text-3xl md:text-2xl mr-4 text-[24px] font-[sentient]">
-              ARSTY.
-            </h1>
+            <div className="lg:pl-[120px] font-bold">
+              <h1 className="lg:text-3xl md:text-2xl mr-4 text-[24px] font-[sentient]">
+                ARSTY.
+              </h1>
+            </div>
+          </div>
+
+          <div className="md:hidden" onClick={handleClick}>
+            {nav ? <AiOutlineClose size={26} /> : <AiOutlineMenu size={26} />}
           </div>
         </div>
 
-         <div className="md:hidden" onClick={handleClick}>
-            {nav ? <AiOutlineClose size={26} /> : <AiOutlineMenu size={26} />}
-        </div>
-        </div>
-       
-        <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px]  text-[#292929] w-[65px] h-[32px]">
-          <Link to="/">Home</Link>
+        <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px]  text-[#292929] w-[65px] h-[52px] mt-[50px]">
+          <Link onClick={handleClose} to="/">
+            Home
+          </Link>
         </li>
-        <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px] text-[#292929] w-[129px] h-[32px]">
-          <Link to="/marketPlace">MarketPlace</Link>
+        <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px] text-[#292929] w-[94px] h-[52px] mt-[18px]">
+          <Link onClick={handleClose} to="/auction">
+            Auctions
+          </Link>
         </li>
-        <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px] text-[#292929] w-[91px] h-[32px]">
-          <Link to="/auction">Auctions</Link>
+        <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px] text-[#292929] w-[134px] h-[52px] mt-[24px]">
+          <Link onClick={handleClose} to="/marketplace">
+            MarketPlace
+          </Link>
         </li>
-        <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px] text-[#292929] w-[53px] h-[32px]">
-          <Link to="/drop">Drop</Link>
+        <li className="cursor-pointer font-[500] leading-[32px] lg:text-[24px] md:text-[19px] text-[#292929] w-[65px] h-[52px] mt-[18px]">
+          <Link onClick={handleClose} to="/drop">
+            Drop
+          </Link>
         </li>
       </ul>
     </div>
