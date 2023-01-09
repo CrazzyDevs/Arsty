99592773618c964fef45814e13af0fa34b7512c1
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2023-01-09 21:26:23 +0100

    Chores: created market folder in pages and move Market.jsx inside the folder

--- src/pages/MarketPlace.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MarketPlace = () => {
-  return <div>MarketPlace</div>;
-};
-
-export default MarketPlace;
---
+++ src/pages/marketPlace/MarketPlace.jsx
@@ -0,0 +1,14 @@
+const MarketPlace = () => {
+  return (
+    <div className="md:px-8 flex items-center justify-center w-full">
+      <div className="w-2/5">
+        <input type="text" placeholder="search" name="" id="" />
+      </div>
+      <div className="w-3/5 px-3 flex items-center justify-between">
+        <p>See 1-6 0f 15 results</p>
+      </div>
+    </div>
+  );
+};
+
+export default MarketPlace;
