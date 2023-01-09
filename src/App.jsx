aae0b245cb8b069daeab3594102d8a6688129c8b
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2023-01-09 21:22:49 +0100

    Chores: rewrote the link of market place and removed import react

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'
-import MarketPlace from './pages/MarketPlace';
-import Auction from './pages/auction/Auction';
-import Drop from './pages/Drop';
-import Layout from './Components/Layout';
-import Error from './pages/Error';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MarketPlace from "./pages/marketPlace/MarketPlace";
+import Auction from "./pages/auction/Auction";
+import Drop from "./pages/Drop";
+import Layout from "./Components/Layout";
+import Error from "./pages/Error";
 
 function App() {
-
   return (
-    <div> 
+    <div>
       <Router>
         <Routes>
-          <Route path='/' element={<Layout/>}>
-            <Route index element={<Home/>} />
-            <Route path="marketplace" element={ <MarketPlace/> } />
-            <Route path="auction" element={ <Auction/> } />
-            <Route path="drop" element={ <Drop/> } />
-            <Route path="*" element={ <Error/> } />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="marketplace" element={<MarketPlace />} />
+            <Route path="auction" element={<Auction />} />
+            <Route path="drop" element={<Drop />} />
+            <Route path="*" element={<Error />} />
           </Route>
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
