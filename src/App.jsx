5ee510524fee68e595262c98287f1f3ce75f9b46
preshpi <egwuenuprecious7@gamil.com>
2023-01-10 06:23:45 +0100

    feat:homepage slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
 import MarketPlace from "./pages/marketPlace/MarketPlace"
 import Auction from './pages/auction/Auction';
 import Drop from './pages/Drop';
