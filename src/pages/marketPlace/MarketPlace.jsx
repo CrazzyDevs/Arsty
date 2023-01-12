257fb9bf809d1e601a746b1d6afd92e58ce2b79c
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2023-01-12 17:20:55 +0100

    feat: completed the marketPlace web design but not responsive yet

--- src/pages/marketPlace/MarketPlace.jsx
+++ src/pages/marketPlace/MarketPlace.jsx
@@ -1,10 +1,39 @@
 import { FiSearch } from "react-icons/fi";
-import { RiArrowDownSLine } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
 import { MdKeyboardArrowUp } from "react-icons/md";
 import { BsFilter } from "react-icons/bs";
+import { BsCheck } from "react-icons/bs";
 import { productsArr } from "./data";
 
 const MarketPlace = () => {
+  const cartArr = [
+    {
+      id: 1,
+      name: "editorial",
+      checked: true,
+    },
+    {
+      id: 2,
+      name: "fashion",
+      checked: true,
+    },
+    {
+      id: 3,
+      name: "optics",
+      checked: false,
+    },
+    {
+      id: 4,
+      name: "art & museum",
+      checked: false,
+    },
+    {
+      id: 5,
+      name: "nature",
+      checked: false,
+    },
+  ];
+
   return (
     <div className="w-full">
       <div className="flex items-center justify-between w-full my-8">
@@ -18,11 +47,9 @@ const MarketPlace = () => {
         </div>
         <div className="w-3/4 px-8 flex items-center justify-between shadow-3xl rounded-xl h-16">
           <p className="text-pureBlack">See 1-6 0f 15 results</p>
-          <div className="relative flex items-center">
-            <select className="border  w-32 py-2 rounded-lg font-medium pl-7 appearance-none cursor-pointer focus:outline-none">
-              <option value="sortBy">Sort by</option>
-            </select>
-            <RiArrowDownSLine className="absolute right-7 text-xl" />
+          <div className="flex items-center border border-darkGrey w-32 py-2 rounded-lg cursor-pointer space-x-2 justify-center">
+            <p className="font-semibold text-pureBlack">Sort by</p>
+            <MdKeyboardArrowDown className="text-2xl" />
           </div>
         </div>
       </div>
@@ -33,22 +60,87 @@ const MarketPlace = () => {
               <BsFilter className="text-2xl" />
               <h1 className="text-xl font-semibold">Filter</h1>
             </div>
-            <div className="">
-              <div className="flex items-center justify-between cursor-pointer mb-8">
+            <div className="space-y-8">
+              <div className="flex items-center justify-between cursor-pointer">
                 <p className="font-semibold text-lg text-pureBlack">
                   By category
                 </p>
                 <MdKeyboardArrowUp className="text-2xl text-pureBlack" />
+                {/* <MdKeyboardArrowDown className="text-2xl text-pureBlack" />  */}
               </div>
               <div className="space-y-3">
-                <div className="flex items-center space-x-3">
-                  <input
-                    type="checkbox"
-                    className="accent-{pink} w-4 h-4 focus:accent-pink-500"
-                    // checked
-                  />
-                  <label className="text-black">Editorials</label>
+                {cartArr.map(({ id, name, checked }) => {
+                  return (
+                    <div className="flex items-center relative" key={id}>
+                      <input
+                        type="checkbox"
+                        className="w-4 h-4 opacity-0 absolute cursor-pointer"
+                        value={name}
+                      />
+                      <div className="w-5 h-5 bg-[#D9D9D9] mr-3 flex items-center rounded cursor-pointer">
+                        {checked && (
+                          <BsCheck className="text-pureBlack text-xl" />
+                        )}
+                      </div>
+                      <label className="text-black capitalize">{name}</label>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="flex items-center justify-between cursor-pointer mb-8">
+                <p className="font-semibold text-lg text-pureBlack">By price</p>
+                <MdKeyboardArrowUp className="text-2xl text-pureBlack" />
+                {/* <MdKeyboardArrowDown className="text-2xl text-pureBlack" />  */}
+              </div>
+              <div className="space-y-6">
+                <p className="text-black">$100.00 - $150.00</p>
+                <div className="flex items-center relative">
+                  <div className="w-full h-1.5 bg-[#B8BCB5] absolute z-10"></div>
+                  <div className="w-[55%] h-1.5 bg-confusedBlack absolute z-10"></div>
+                  <div className="w-6 h-6 border-2 z-20 border-confusedBlack bg-[white] rounded-full"></div>
+                  <div className="w-6 h-6 ml-[55%] absolute z-30 bg-confusedBlack rounded-full cursor-pointer"></div>
                 </div>
+              </div>
+              <div className="flex items-center justify-between cursor-pointer">
+                <p className="font-semibold text-lg text-pureBlack">
+                  By artist
+                </p>
+                <MdKeyboardArrowUp className="text-2xl text-pureBlack" />
+                {/* <MdKeyboardArrowDown className="text-2xl text-pureBlack" />  */}
+              </div>
+              <div className="space-y-3">
+                <p className="border-b border-b-pureBlack inline">All</p>
+                <button
+                  value={100}
+                  className="capitalize bg-[transparent] border-none block"
+                >
+                  below $100.00
+                </button>
+                <button
+                  value={150}
+                  className="capitalize bg-[transparent] border-none block"
+                >
+                  $100.00 - $150.00{" "}
+                </button>
+                <button
+                  value={200}
+                  className="capitalize bg-[transparent] border-none block"
+                >
+                  $150.00 - $200.00{" "}
+                </button>
+                <button
+                  value={201}
+                  className="capitalize bg-[transparent] border-none block"
+                >
+                  above $200.00{" "}
+                </button>
+              </div>
+              <div className="flex items-center justify-between cursor-pointer">
+                <p className="font-semibold text-lg text-pureBlack">
+                  Collection year
+                </p>
+                {/* <MdKeyboardArrowUp className="text-2xl text-pureBlack" /> */}
+                <MdKeyboardArrowDown className="text-2xl text-pureBlack" /> 
               </div>
             </div>
           </div>
